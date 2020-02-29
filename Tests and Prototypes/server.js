2fea24658d8ff198d4feec22bb6daf9f05f57d58
MattRiina <matthewriina@gmail.com>
2020-02-28 20:52:10 -0500

    Got class to work in Node.js for Tweets

--- Tests and Prototypes/server.js
+++ Tests and Prototypes/server.js
@@ -1,20 +1,28 @@
 console.log("Test started")
 var Twit = require('twit');
 var config = require('./config')
+const Tweet = require('./tweet').Tweet 
+//import Tweet from './tweet'
 var T = new Twit(config);
 var params = { 
     q: 'Google',
     count: 100
 }
-
+//const tweet = new Tweet("", "","","","","","");
+//console.log(tweet);
 T.get('search/tweets', params, searchedData);
 
 function searchedData(err, data, response) {
     //console.log(data);
+    var tweets = [];
     // Loop through the returned tweets
     for(let i = 0; i < data.statuses.length; i++) {
         // Get the tweet from the returned data
-        let tweet = { text: data.statuses[i].text }
-        console.log(tweet)
+        //let tweetText = { text: data.statuses[i].text }
+        let tweet = data.statuses[i];
+        tweets.push(new Tweet(tweet.created_at, tweet.id, tweet.text, tweet.entities, tweet.user, tweet.retweet_count, tweet.favorite_count));
+        //console.log(tweetText)
     }
+
+    console.log(tweets[0])
 }
