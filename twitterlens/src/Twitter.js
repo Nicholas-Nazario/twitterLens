6ef7343639c15c637c8b87200161c0b42b069641
MattRiina <matthewriina@gmail.com>
2020-03-29 17:26:15 -0400

    Started to merge code

--- twitterlens/src/Twitter.js
+++ twitterlens/src/Twitter.js
@@ -9,32 +9,45 @@ var Twit = require('twit');
 var config = require('./config')
 const Tweet = require('./tweet').Tweet 
 
-function searchTwitter(){
-  var T = new Twit(config);
-  const params = { 
-    q: 'Google',
-    language: 'en',
-    count: 100    
+// function searchTwitter(){
+//   var T = new Twit(config);
+//   const params = { 
+//     q: 'Google',
+//     language: 'en',
+//     count: 100    
+//   }
+//   T.get('search/tweets', params, searchedData);
+// }
+
+
+// function searchedData(err, data, response) {
+//   //console.log(data);
+//   var tweets = [];
+//   // Loop through the returned tweets
+//   for(let i = 0; i < data.statuses.length; i++) {
+//     // Get the tweet from the returned data
+//     //let tweetText = { text: data.statuses[i].text }
+//     let tweet = data.statuses[i];
+//     tweets.push(new Tweet(tweet.created_at, tweet.id, tweet.text, tweet.entities, tweet.user, tweet.retweet_count, tweet.favorite_count));
+//     //console.log(tweetText)
+//   }
+//   console.log(tweets[0])
+// }
+
+class Twitter extends React.Component {
+  static async getInitialProps() {
+    let twitterAccessToken;
+    try {
+      twitterAccessToken = await Twitter.obtainAccessToken();
+    } catch (error) {
+      twitterAccessToken = '';
+    }
+
+    return {
+      twitterAccessToken,
+    };
   }
-  T.get('search/tweets', params, searchedData);
-}
 
-
-function searchedData(err, data, response) {
-  //console.log(data);
-  var tweets = [];
-  // Loop through the returned tweets
-  for(let i = 0; i < data.statuses.length; i++) {
-    // Get the tweet from the returned data
-    //let tweetText = { text: data.statuses[i].text }
-    let tweet = data.statuses[i];
-    tweets.push(new Tweet(tweet.created_at, tweet.id, tweet.text, tweet.entities, tweet.user, tweet.retweet_count, tweet.favorite_count));
-    //console.log(tweetText)
-  }
-  console.log(tweets[0])
-}
-
-class Twitter extends Component {
   constructor() {
     super();
     this.state = {
@@ -44,6 +57,53 @@ class Twitter extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  state = {
+    q: '',
+    isLoading: false,
+    tweets: [],
+  };
+
+  searchTwitter(){
+    const q = req.query.q || '@Google';
+
+    const params = {
+      q,
+      include_entities: false,
+      language: 'en',
+      count: 100,
+    };
+
+    axios({
+      url: `https://api.twitter.com/1.1/search/tweets.json?${qs.stringify(
+        params,
+      )}`,
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${req.query.accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(items => {
+        res.json({
+          error: false,
+          items: items.data.statuses,
+        });
+      })
+      .catch(() => {
+        res.json({
+          error: true,
+          message: 'Something went wrong',
+        });
+      });
+  }
+
+  search(){
+    this.setState({
+      isLoading: true,
+    });
+
     
   }
 
@@ -54,19 +114,9 @@ class Twitter extends Component {
   }
 
   handleClick(e) {
-    if (e) e.preventDefault();
-    this.setState ({
-      value: '',
-      term: this.state.value
-    });
+    e.preventDefault();
 
-    //let term = this.state.value;
-    let term = 'Google';
-    const api_key = '${process.env.REACT_APP_STOCK_API_KEY}';
-    console.log(term);
-    
-    const twitter_api = 'https://api.twitter.com/1.1/search/tweets.json?q=${term}&language=en&count=100';
-    // console.log(twitter_api)
+    this.search();
     
     
     // const response = await axios.get(twitter_api)
@@ -77,27 +127,6 @@ class Twitter extends Component {
 
     // console.log(response.data)
 
-    const params = { 
-      q: 'Google',
-      language: 'en',
-      count: 100    
-    };
-
-    axios({
-      url: `https://api.twitter.com/1.1/search/tweets.json?${qs.stringify(
-        params,
-      )}`,
-      method: 'GET',
-      headers: {
-        Authorization: 'OAuth oauth_consumer_key="PWgZm9kGHyw611SWf3AFPhJF7",oauth_token="1222989356992016390-A4oRtuva5nHsZPs0hFgyGz1F06Txh4",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1585175917",oauth_nonce="d8fVIIvhqqP",oauth_version="1.0",oauth_signature="fq5untfEh1d1RSO9Aj2O6KienbI%3D"'
-      },
-    })
-      .then(items => {
-          console.log(items.data.statuses)
-      })
-      .catch(() => {
-        console.log("error occurred")
-      });
   }
 
   render() {
