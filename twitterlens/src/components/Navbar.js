1c24f77acfe5165d58c26bb6b4892166ca2d0f0a
Nicholas Nazario <Nazario9847@gmail.com>
2020-03-29 16:51:07 -0400

    Navbar with proper routing

--- twitterlens/src/components/Navbar.js
+++ twitterlens/src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
             <div className="Navbar">
                 <header className="Navbar-header">
                     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <a class="navbar-brand" href="#">
+                        <a class="navbar-brand" href="/">
                             <img src={Logo} width="60" height="60" alt=""></img>
                         </a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,19 +18,22 @@ class Navbar extends Component {
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item dropdown">
+                            {/* <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Data Pages
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Stock</a>
-                                    <a class="dropdown-item" href="#">Twitter</a>
+                                    <a class="dropdown-item" href="/stock">Stock</a>
+                                    <a class="dropdown-item" href="/twitter">Twitter</a>
                                 </div>
+                            </li> */}
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/about">About Twitter Lens <span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item active">
-                                <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="/team">Dev Team <span class="sr-only">(current)</span></a>
                             </li>
                             </ul>
                         </div>
--- twitterlens/src/index.js
+++ twitterlens/src/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
 import Stock from './Stock';
 // import Twitter from './Twitter';
 import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Team from './components/Team';
 import * as serviceWorker from './serviceWorker';
+
+
+const routing = (
+  <Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Stock} />
+        <Route path="/stock" component={Stock} />
+        <Route path="/about" component={About} />
+        <Route path="/Team" component={Team} />
+        <Route path="/twitter" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
 
 let componentList = (
   <div>
-    <Navbar />
-    <Stock />
+	  <Navbar />
+	  <Stock />
   </div>
 );
 
 ReactDOM.render(
-  componentList,
+  routing,
   document.getElementById('root')
 );
 
