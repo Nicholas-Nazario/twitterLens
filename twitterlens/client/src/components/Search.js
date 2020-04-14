1e0e94e2ca4701db1c527bfb0f33741511c4c0d6
Andrew Emanuel <emanutter@gmail.com>
2020-04-13 23:13:05 -0400

    Made JSON file with all NASDAQ-100 stock tickers and company names.

--- twitterlens/client/src/components/Search.js
+++ twitterlens/client/src/components/Search.js
@@ -2,10 +2,7 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react'
 
-const countryOptions = [
-    { value: 'IBM', text: 'IBM' },
-    { value: 'MSFT', text: "Microsoft" },
-]
+const companyOptions = require('./Nasdaq.json');
 
 
 
@@ -18,57 +15,21 @@ export default class SearchBar extends React.Component {
         this.state = {value: ''}
     }
 
-    // handleSelectChange = (event, data) => {
-    //     this.setState({
-    //         [data.name]: data.value
-    //     })
-    // }
-
     //Return the below HTML code and export as a SearchBar object
     //This allows a specific component (ex. Stock.js) render it within the page
     render() {
         return (
             <div className="search_bar">
                 <Dropdown className="search_input"
-                    placeholder='Select a Stock'
-                    fluid
+                    placeholder='Select a Company'
                     search
                     selection
-                    options={countryOptions}
+                    options={companyOptions}
 
                     onChange={this.props.onClick}
                     onSearchChange={this.props.onChange}
-                    
-
-                    // value={this.state.value}
-
-                    // onChange={this.props.handleSelectChange}
-
-                    //value ={value}
-                    
-                    //onClose={this.props.onChange}
-                    //onClose={this.props.onClick}
                 />
-                <button className="search_button" onClick={this.props.onClick}>Stock Search</button>
             </div>
         );
     };
-    
-    // return (
-    //         <div className="search_bar">
-    //             <form className = "search_bar_form">
-    //                 {/* 
-    //                     value = text (type String) that is input by the user
-    //                         - This is returned as an event in your specified onChange function
-    //                     onChange = specified function that will change the state of a variable in 
-    //                         a component to be the value from above
-    //                     onClick = specified function that will do an action after the button is clicked
-    //                  */}
-    //                 <input type="text" className="search_input"
-    //                     value={props.value}
-    //                     onChange={props.onChange}/>
-    //                 <button className="search_button" onClick={props.onClick}>Stock Search</button>
-    //             </form>
-    //         </div> 
-    // );
 }
