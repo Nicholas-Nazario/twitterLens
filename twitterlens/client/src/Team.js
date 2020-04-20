8ccd3a9cc232e408f01d2e3a9a278eab1a55a2f1
MattRiina <matthewriina@gmail.com>
2020-04-20 00:35:46 -0400

    finished refactoring Team.js

--- twitterlens/client/src/Team.js
+++ twitterlens/client/src/Team.js
@@ -21,120 +21,117 @@ import LogoBack from './headshots/lensBack.jpg';
 // It extends the react Component inheriting essetial react functionality such as render()
 // Design Inspired by https://codepen.io/miketricking/pen/JXdzpL
 class Team extends Component {
-    // Render the below HTML code and export as Team
-    // This lets the index.js read the HTML as a single object 
-    render() {
-        return (
-            <div className="Team">
-            <header className="Team-header">
-            <div className="container">
-                <div className="row ">
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={RiinaBack} alt="Background Matthew Riina"></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Riina} alt="Matthew Riina"></img>
-                                <h2 className="hvr-underline-from-center">Matthew Riina<span>Product Owner / Twitter Developer</span></h2>
-                                <div>
-                                    Matthew is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2022 with a concentration in Artificial Intelligence and Data. His work experience includes an internship as a Software Developer at IBM. Matthew is currently looking for opportunities for his Arch away semester starting January 2021.
-                                </div>
-                                <a href="https://www.instagram.com/mattriina/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
-                                <a href="https://github.com/MattRiina" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                                <a href="http://www.linkedin.com/in/matthew-riina-19a394178" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
-                            </div>
-                        </div>
+  // Render the below HTML code and export as Team
+  // This lets the index.js read the HTML as a single object 
+  render() {
+    return (
+      <div className="Team">
+        <header className="Team-header">
+          <div className="container">
+            <div className="row ">
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                  <img className="img-responsive"  src={RiinaBack} alt="Background Matthew Riina"></img>
+                  <div className="profile-info">
+                    <img className="profile-pic" src={Riina} alt="Matthew Riina"></img>
+                    <h2 className="hvr-underline-from-center">Matthew Riina<span>Product Owner / Twitter Developer</span></h2>
+                    <div>
+                        Matthew is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2022 with a concentration in Artificial Intelligence and Data. His work experience includes an internship as a Software Developer at IBM. Matthew is currently looking for opportunities for his Arch away semester starting January 2021.
                     </div>
-
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={NickBack} alt="Background Nicholas Nazario"></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Nick} alt="Nicholas Nazario"></img>
-                                <h2 className="hvr-underline-from-center">Nick Nazario<span>Scrum Master / UX Developer</span></h2>
-                                <div>
-                                    Nick is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2020 with a concentraion in Artificial Intelligence and Data. His work experience includes internships as a Software Developer at MITRE, MIT Lincoln Laboratory, and Boeing. Nick will be starting a full time position with Amazon as a SDE in September 2020.
-                                </div>
-                                <a href="https://twitter.com/nick_nazario" target="_blank"><i className="fab fa-twitter fa-2x"></i></a>
-                                <a href="https://github.com/nicholas-nazario" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                                <a href="https://www.linkedin.com/in/nicholas-nazario/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={AndyBack} alt="Background Andrew Emanuel"></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Andy} alt="Andrew Emanuel"></img>
-                                <h2 className="hvr-underline-from-center">Andrew Emanuel<span>Visualization Developer</span></h2>
-                                <div>
-                                    Andrew is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2022 with a concentration in either AI and Data or Systems and Software. He is currently seeking opportunities starting in January 2021 for his Arch semester away.
-                                </div>
-                                <a href="https://www.instagram.com/andy.emanuel/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
-                                <a href="https://github.com/emanua" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                                <a href="https://www.linkedin.com/in/andrew-emanuel-60b757139/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
-                            </div>
-                        </div>
-                    </div>
+                    <a href="https://www.instagram.com/mattriina/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                    <a href="https://github.com/MattRiina" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="http://www.linkedin.com/in/matthew-riina-19a394178" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                  </div>
                 </div>
-                <div className="row ">
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={DanBack} alt="Background Daniel Peladeau"></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Dan} alt="Daniel Peladeau"></img>
-                                <h2 className="hvr-underline-from-center">Daniel Peladeau<span>Stock Developer</span></h2>
-                                <div>
-                                    Daniel is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2020 with a concentration in Artificial Intelligence and Data. His work experience includes internships as a Software Developer at Hayward Industries and Professional Services Intern at Amazon Web Services. Daniel will be starting a full time position with AWS as a Professional Services Consultant in August of 2020.
-                                </div>
-                                <a href="https://www.instagram.com/dcpeladeau/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
-                                <a href="https://github.com/Daniel-Peladeau" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                                <a href="https://www.linkedin.com/in/daniel-peladeau/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
-                            </div>
-                        </div>
-                    </div>
+              </div>
 
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={SoloBack} alt="Background Matthew Solomon"></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Solomon} alt="Matthew Solomon"></img>
-                                <h2 className="hvr-underline-from-center">Matt Solomon<span>Database Developer</span></h2>
-                                <div>
-                                Matthew Solomon is a computer science student at Rensselaer Polytechnic Institute graduating spring of 2021. His experiences include one internship at Microsoft.
-                                </div>
-                                <a href="https://www.instagram.com/matthewsolomon/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
-                                <a href="https://github.com/Matthew-Solomon" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                                <a href="https://www.linkedin.com/in/matthew-solomon-667938167" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
-                            </div>
-                        </div>
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                  <img className="img-responsive"  src={NickBack} alt="Background Nicholas Nazario"></img>
+                  <div className="profile-info">
+                    <img className="profile-pic" src={Nick} alt="Nicholas Nazario"></img>
+                    <h2 className="hvr-underline-from-center">Nick Nazario<span>Scrum Master / UX Developer</span></h2>
+                    <div>
+                        Nick is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2020 with a concentraion in Artificial Intelligence and Data. His work experience includes internships as a Software Developer at MITRE, MIT Lincoln Laboratory, and Boeing. Nick will be starting a full time position with Amazon as a SDE in September 2020.
                     </div>
-
-                    <div className="col-md-4">
-                        <div className="profile-card text-center">
-                        <img className="img-responsive"  src={LogoBack} alt="Background Twitter Lens Dev
-                        
-                        
-                        "></img>
-                            <div className="profile-info">
-                                <img className="profile-pic" src={Logo} alt="Twitter Lens Dev"></img>
-                                <h2 className="hvr-underline-from-center">Twitter Lens<span>Dev Team</span></h2>
-                                <div>
-                                    Contact The Team!
-                                </div>
-                                <div>
-                                TwitterLens is a tool that utilizes live stock market data in tandem with relevant Tweets from Twitter in order to inform investors and day traders of live market sentiment.
-                                </div>
-                                <a href="https://twitter.com/dev_lens" target="_blank"><i className="fa fa-twitter fa-2x"></i></a>
-                                <a href="mailto:twitterlensdev@gmail.com"><i className="fa fa-envelope-o fa-2x"></i></a>
-                            </div>
-                        </div>
-                    </div>
+                    <a href="https://twitter.com/nick_nazario" target="_blank"><i className="fab fa-twitter fa-2x"></i></a>
+                    <a href="https://github.com/nicholas-nazario" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/nicholas-nazario/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                  </div>
                 </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                  <img className="img-responsive"  src={AndyBack} alt="Background Andrew Emanuel"></img>
+                  <div className="profile-info">
+                    <img className="profile-pic" src={Andy} alt="Andrew Emanuel"></img>
+                    <h2 className="hvr-underline-from-center">Andrew Emanuel<span>Visualization Developer</span></h2>
+                    <div>
+                        Andrew is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2022 with a concentration in either AI and Data or Systems and Software. He is currently seeking opportunities starting in January 2021 for his Arch semester away.
+                    </div>
+                    <a href="https://www.instagram.com/andy.emanuel/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                    <a href="https://github.com/emanua" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/andrew-emanuel-60b757139/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                  </div>
+                </div>
+              </div>
             </div>
-            </header>
+            <div className="row ">
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                  <img className="img-responsive"  src={DanBack} alt="Background Daniel Peladeau"></img>
+                  <div className="profile-info">
+                    <img className="profile-pic" src={Dan} alt="Daniel Peladeau"></img>
+                    <h2 className="hvr-underline-from-center">Daniel Peladeau<span>Stock Developer</span></h2>
+                    <div>
+                        Daniel is a Computer Science student at Rensselaer Polytechnic Institute graduating in 2020 with a concentration in Artificial Intelligence and Data. His work experience includes internships as a Software Developer at Hayward Industries and Professional Services Intern at Amazon Web Services. Daniel will be starting a full time position with AWS as a Professional Services Consultant in August of 2020.
+                    </div>
+                    <a href="https://www.instagram.com/dcpeladeau/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                    <a href="https://github.com/Daniel-Peladeau" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/daniel-peladeau/" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                <img className="img-responsive"  src={SoloBack} alt="Background Matthew Solomon"></img>
+                  <div className="profile-info">
+                    <img className="profile-pic" src={Solomon} alt="Matthew Solomon"></img>
+                    <h2 className="hvr-underline-from-center">Matt Solomon<span>Database Developer</span></h2>
+                    <div>
+                        Matthew Solomon is a computer science student at Rensselaer Polytechnic Institute graduating spring of 2021. His experiences include one internship at Microsoft.
+                    </div>
+                    <a href="https://www.instagram.com/matthewsolomon/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                    <a href="https://github.com/Matthew-Solomon" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/matthew-solomon-667938167" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="profile-card text-center">
+                  <img className="img-responsive"  src={LogoBack} alt="Background Twitter Lens Dev"></img>
+                  <div className="profile-info">
+                      <img className="profile-pic" src={Logo} alt="Twitter Lens Dev"></img>
+                      <h2 className="hvr-underline-from-center">Twitter Lens<span>Dev Team</span></h2>
+                      <div>
+                          Contact The Team!
+                      </div>
+                      <div>
+                      TwitterLens is a tool that utilizes live stock market data in tandem with relevant Tweets from Twitter in order to inform investors and day traders of live market sentiment.
+                      </div>
+                      <a href="https://twitter.com/dev_lens" target="_blank"><i className="fa fa-twitter fa-2x"></i></a>
+                      <a href="mailto:twitterlensdev@gmail.com"><i className="fa fa-envelope-o fa-2x"></i></a>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 export default Team;
